fdd6b878d4a0cc2d441ec7dc703647f5691cbb7a
León <argon5571@gmail.com>
2025-08-19 17:28:44 -0300

    Adjusted Split operation to always distribute resources in multiples of 5.

--- Source/ArgonicCore/ArgonicCore/Utilities/SplitUtility.cs
+++ Source/ArgonicCore/ArgonicCore/Utilities/SplitUtility.cs
@@ -1,76 +1,28 @@
-﻿namespace ArgonicCore.Utilities
+﻿using System.Configuration;
+
+namespace ArgonicCore.Utilities
 {
     public static class SplitUtility
     {
+        private static int RoundToNearest5(int value)
+        {
+            int roundedValue = (value + 2) / 5 * 5;
+            return roundedValue == 0 ? 5 : roundedValue;
+        }
+
         public static int Split(string splitMode, int amount, out int extracted)
         {
             if (splitMode == "small")
             {
-                switch (amount)
-                {
-                    case 15:
-                        extracted = 5;
-                        return 10;
-                    case 20:
-                        extracted = 5;
-                        return 15;
-                    case 25:
-                        extracted = 10;
-                        return 15;
-                    case 30:
-                        extracted = 10;
-                        return 20;
-                    case 35:
-                        extracted = 15;
-                        return 20;
-                    case 40:
-                        extracted = 15;
-                        return 25;
-                    case 60:
-                        extracted = 20;
-                        return 40;
-                    case 80:
-                        extracted = 30;
-                        return 50;
-                    default:
-                        extracted = amount / 3;
-                        if (extracted <= 0) { extracted = 1; }
-                        return amount - extracted;
-                }
+                extracted = RoundToNearest5(amount / 3);
+                if (extracted > amount) extracted = amount;
+                return amount - extracted;
             }
             if (splitMode == "big")
             {
-                switch (amount)
-                {
-                    case 15:
-                        extracted = 10;
-                        return 5;
-                    case 20:
-                        extracted = 15;
-                        return 5;
-                    case 25:
-                        extracted = 15;
-                        return 10;
-                    case 30:
-                        extracted = 20;
-                        return 10;
-                    case 35:
-                        extracted = 20;
-                        return 15;
-                    case 40:
-                        extracted = 25;
-                        return 15;
-                    case 60:
-                        extracted = 40;
-                        return 20;
-                    case 80:
-                        extracted = 50;
-                        return 30;
-                    default:
-                        extracted = amount / 2;
-                        if (extracted <= 0) { extracted = 1; }
-                        return amount - extracted;
-                }
+                extracted = RoundToNearest5(amount / 2);
+                if (extracted > amount) extracted = amount;
+                return amount - extracted;
             }
             if (splitMode == "replace")
             {
